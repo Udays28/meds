61aed851f1ccc191932acf40d0e8d12bb2151607
ARYAN TYAGI <aryantyagi111s@gmail.com>
2024-04-29 16:13:51 +0530

    portfolio updated

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 
 const Portfolio = () => {
   // State to store information of the clicked portfolio item
--- src/pages/PortfolioWork.jsx
+++ src/pages/PortfolioWork.jsx
@@ -1,6 +1,37 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
 
 const PortfolioWork = () => {
+  const [webdev, setWebdev] = useState([]);
+  const [socialMediaMarketing, setsocialMediaMarketing] = useState([
+    {
+      title: "MK Traders",
+      image: "./projectimages/mktraders.jpg",
+      client: "Client 3",
+      date: "March 2022",
+    },
+    {
+      title: "MK Traders",
+      image: "./projectimages/mktraders.jpg",
+      client: "Client 3",
+      date: "March 2022",
+    },
+    {
+      title: "MK Traders",
+      image: "./projectimages/mktraders.jpg",
+      client: "Client 3",
+      date: "March 2022",
+    },
+    {
+      title: "MK Traders",
+      image: "./projectimages/mktraders.jpg",
+      client: "Client 3",
+      date: "March 2022",
+    },
+  ]);
+  const [facebookads, setFacebookAds] = useState([]);
+  const [logoDesigning, setLogoDesigning] = useState([]);
+  const [videoEditing, setvideoEditing] = useState([]);
   return (
     <div className="lg:px-28 p-4 mt-1">
       <div className="md:w-2/3 lg:w-1/2 mt-12 text-black">
@@ -12,92 +43,15 @@ const PortfolioWork = () => {
         </p>
       </div>
 
-      <div className="carousel mt-12 rounded-box">
-        <div className="carousel-item">
-          <img
-            src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg"
-            alt="Burger"
-          />
-        </div>
-        <div className="carousel-item">
-          <img
-            src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg"
-            alt="Burger"
-          />
-        </div>
-        <div className="carousel-item">
-          <img
-            src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg"
-            alt="Burger"
-          />
-        </div>
-        <div className="carousel-item">
-          <img
-            src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg"
-            alt="Burger"
-          />
-        </div>
-        <div className="carousel-item">
-          <img
-            src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg"
-            alt="Burger"
-          />
-        </div>
-        <div className="carousel-item">
-          <img
-            src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg"
-            alt="Burger"
-          />
-        </div>
-        <div className="carousel-item">
-          <img
-            src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg"
-            alt="Burger"
-          />
-        </div>
-      </div> <div className="carousel mt-12 rounded-box">
-        <div className="carousel-item">
-          <img
-            src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg"
-            alt="Burger"
-          />
-        </div>
-        <div className="carousel-item">
-          <img
-            src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg"
-            alt="Burger"
-          />
-        </div>
-        <div className="carousel-item">
-          <img
-            src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg"
-            alt="Burger"
-          />
-        </div>
-        <div className="carousel-item">
-          <img
-            src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg"
-            alt="Burger"
-          />
-        </div>
-        <div className="carousel-item">
-          <img
-            src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg"
-            alt="Burger"
-          />
-        </div>
-        <div className="carousel-item">
-          <img
-            src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg"
-            alt="Burger"
-          />
-        </div>
-        <div className="carousel-item">
-          <img
-            src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg"
-            alt="Burger"
-          />
-        </div>
+      <h1 className=" mt-12 my-4 text-black text-3xl font-semibold">
+        Social Media Marketing
+      </h1>
+      <div className="carousel carousel-center bg-zinc-200 p-4 space-x-4  rounded-box">
+        {socialMediaMarketing.map((items, index) => (
+          <div key={index} className="carousel-item">
+            <img className="h-96" src={items.image} alt="Burger" />
+          </div>
+        ))}
       </div>
     </div>
   );
